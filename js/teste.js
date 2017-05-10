79f928443dcc2e9d944e77aeafb77ba19ca1cb99
whuaman <willdebuena@gmail.com>
2017-05-10 12:41:25 -0300

    add banner rotative

---
+++ js/teste.js
@@ -0,0 +1,16 @@
+
+function Leads2w (){
+		{
+			"nome_portal": "landing_page2W",
+			"reference": "9763", 
+			"codigoEmpresa": 240,
+			"incorporador": false, 
+		 
+			"nomeCampanha": "minha_campanha", 
+		    "interessado": { 
+		    	"email": "teste-integracao@teste.com", 
+		    	"nome": "Teste Integracao" 
+				}
+		  "mensagem": "Olá, gostaria de ter mais informações sobre Helbor Family Garden. Aguardo seu contato,obrigado."
+		}
+}
